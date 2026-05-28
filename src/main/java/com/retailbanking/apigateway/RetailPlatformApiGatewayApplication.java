03e89642e9dd4c0c08ee2bb87c52f55d6c0f650e
Bhaskar Dasari <bhaskardasari3004@gmail.com>
2026-05-28 13:32:25 +0400

    src added

---
+++ src/main/java/com/retailbanking/apigateway/RetailPlatformApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.retailbanking.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailPlatformApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailPlatformApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retailbanking/apigateway/RetailPlatformApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailbanking.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailPlatformApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
